f57dd0f6a5d6cfae068c18670e433be6a3159c46
BMike <cypher3204@gmail.com>
2025-02-23 18:26:52 +0700

    catch not ok response error 2

--- src/api/get-ingredients.ts
+++ src/api/get-ingredients.ts
@@ -3,7 +3,7 @@ import { BASE_URL } from '@utils/constants';
 const getIngredients = async () => {
 	const response = await fetch(`${BASE_URL}ingredients`);
 	if (!response.ok) {
-		throw new Error('Ошибка запроса!');
+		return Promise.reject(`Ошибка ${response.status}`);
 	}
 	const result = await response.json();
 	return result.data;
