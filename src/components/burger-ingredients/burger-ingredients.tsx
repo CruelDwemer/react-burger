74616cd370467ff8cfacd640f2018b9d364a8617
BMike <cypher3204@gmail.com>
2025-03-04 01:11:37 +0700

    feature scroll by click on tab

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
+import { useRef, useState, useEffect } from 'react';
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
-import { useState } from 'react';
 import styles from './styles.module.scss';
-import { TAB, ITab } from './types';
+import { ITab, TAB } from './types';
 import { DataInterface } from '../../types';
 import IngredientsBlock from './components/ingredients-block';
 import Modal from '../modal';
@@ -27,13 +27,49 @@ const tabs: ITab[] = [
 
 const BurgerIngredients = () => {
 	const [selectedTab, setSelectedTab] = useState<TAB>(TAB.BUN);
-	const selectTab = (value: TAB) => setSelectedTab(value);
 
 	const [selectedIngredient, setSelectedIngredient] =
 		useState<DataInterface | null>(null);
 	const closeModal = setSelectedIngredient.bind(null, null);
 	const selectIngredient = (ingredient: DataInterface) =>
 		setSelectedIngredient(ingredient);
+
+	const containerRef = useRef<HTMLDivElement | null>(null);
+
+	const selectTab = (value: TAB) => {
+		setSelectedTab(value);
+		const sections = containerRef?.current?.querySelectorAll('.section') || [];
+		if(sections) {
+			const target = Array.from(sections).find(section => section.getAttribute('data-type') === value);
+			if(target) {
+				const { top } = target.getBoundingClientRect();
+				console.log(value, top)
+				target.scrollIntoView({ behavior: 'smooth' });
+			}
+		}
+
+	};
+
+	function handleScroll() {
+		const sections = containerRef?.current?.querySelectorAll('.section') || [];
+		let index = 0;
+
+		sections.forEach((section, i) => {
+			const { top } = section.getBoundingClientRect();
+			if (top <= 300) {
+				index = i;
+			}
+		});
+		setSelectedTab(sections[index].getAttribute('data-type') as TAB);
+	}
+
+	useEffect(() => {
+		const container = containerRef.current;
+		if(container) {
+			container.addEventListener('scroll', handleScroll);
+			return () => container.removeEventListener('scroll', handleScroll);
+		}
+	}, [])
 
 	const IngredientTab = ({ value, label }: ITab) => (
 		<Tab
@@ -53,14 +89,14 @@ const BurgerIngredients = () => {
 					<IngredientDetails {...selectedIngredient} />
 				</Modal>
 			)}
-			<section>
+			<section >
 				<h1 className={styles.header}>Соберите бургер</h1>
 				<div className={styles.tabs}>
 					{tabs.map((el, index) => (
 						<IngredientTab key={index} {...el} />
 					))}
 				</div>
-				<div className={styles.content}>
+				<div className={styles.content} ref={containerRef}>
 					{tabs.map((tab, index) => {
 						const filteredData = ingredients.filter(({ type }) => type === tab.value);
 						return (
@@ -69,6 +105,7 @@ const BurgerIngredients = () => {
 								header={tab.label}
 								data={filteredData}
 								onClick={selectIngredient}
+								type={tab.value}
 							/>
 						);
 					})}
--- src/components/burger-ingredients/components/ingredients-block/ingredients-block.tsx
+++ src/components/burger-ingredients/components/ingredients-block/ingredients-block.tsx
@@ -2,22 +2,24 @@ import * as React from 'react';
 import Ingredient from './ingredient';
 import { DataInterface } from '../../../../types';
 import styles from './styles.module.scss';
+import {TAB} from '../../types';
 
 interface Props {
 	header: string;
 	data: DataInterface[];
 	onClick: (data: DataInterface) => void;
+	type: TAB;
 }
 
-const IngredientsBlock = ({ header, data, onClick }: Props) => (
-	<>
+const IngredientsBlock = ({ header, data, onClick, type }: Props) => (
+	<section className='section' data-type={type}>
 		<h2 className={styles.header}>{header}</h2>
 		<div className={styles.content}>
 			{data.map((item) => (
 				<Ingredient key={item._id} {...item} onClick={onClick.bind(null, item)} />
 			))}
 		</div>
-	</>
+	</section>
 );
 
 export default IngredientsBlock;
